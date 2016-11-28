5ac3c6533a8e155747a7f200eca0d3d3330efa8c
Daniel Wolf <danielrwolf5@gmail.com>
2016-11-28 09:29:30 -0600

    converting tests to python

--- tests/matching_test.py
+++ tests/matching_test.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from zxcvbn import adjacency_graphs
 from zxcvbn import matching
 
 
@@ -250,7 +251,7 @@ def l33t_matching():
                           'matched_word': [word],
                           'rank': [rank],
                           'dictionary_name': [dictionary_name],
-                          })
+                      })
 
     matches = lm('@a(go{G0')
     msg = "matches against overlapping l33t patterns"
@@ -279,3 +280,295 @@ def l33t_matching():
     # TODO: consider partially fixing by trying all subsets of size 1 and maybe 2
     msg = "doesn't match with subsets of possible l33t substitutions"
     assert lm('4sdf0') == [], msg
+
+
+def test_spatial_matching():
+    for password in ['', '/', 'qw', '*/']:
+        msg = "doesn't match 1- and 2-character spatial patterns"
+        assert matching.spatial_match(password) == [], msg
+
+    # for testing, make a subgraph that contains a single keyboard
+    _graphs = {'qwerty': adjacency_graphs.qwerty}
+    pattern = '6tfGHJ'
+    matches = matching.spatial_match("rz!#{pattern}%z", _graphs)
+    msg = "matches against spatial patterns surrounded by non-spatial patterns"
+    check_matches(msg, matches, 'spatial', [pattern],
+                  [[3, 3 + len(pattern) - 1]],
+                  {
+                      'graph': ['qwerty'],
+                      'turns': [2],
+                      'shifted_count': [3],
+                  })
+
+    for pattern, keyboard, turns, shifts in [
+        ['12345', 'qwerty', 1, 0],
+        ['@WSX', 'qwerty', 1, 4],
+        ['6tfGHJ', 'qwerty', 2, 3],
+        ['hGFd', 'qwerty', 1, 2],
+        ['/;p09876yhn', 'qwerty', 3, 0],
+        ['Xdr%', 'qwerty', 1, 2],
+        ['159-', 'keypad', 1, 0],
+        ['*84', 'keypad', 1, 0],
+        ['/8520', 'keypad', 1, 0],
+        ['369', 'keypad', 1, 0],
+        ['/963.', 'mac_keypad', 1, 0],
+        ['*-632.0214', 'mac_keypad', 9, 0],
+        ['aoEP%yIxkjq:', 'dvorak', 4, 5],
+        [';qoaOQ:Aoq;a', 'dvorak', 11, 4],
+    ]:
+        _graphs = {'keyboard': adjacency_graphs[keyboard]}
+        matches = matching.spatial_match(pattern, _graphs)
+        msg = "matches '#{pattern}' as a #{keyboard} pattern"
+        check_matches(msg, matches, 'spatial', [pattern],
+                      [[0, len(pattern) - 1]],
+                      {
+                          'graph': [keyboard],
+                          'turns': [turns],
+                          'shifted_count': [shifts],
+                      })
+
+
+def test_sequence_matching():
+    for password in ['', 'a', '1']:
+        msg = "doesn't match length-#{len(password)} sequences"
+        assert matching.sequence_match(password) == [], msg
+
+    matches = matching.sequence_match('abcbabc')
+    msg = "matches overlapping patterns"
+    check_matches(msg, matches, 'sequence', ['abc', 'cba', 'abc'],
+                  [[0, 2], [2, 4], [4, 6]],
+                  {'ascending': [True, False, True]})
+
+    prefixes = ['!', '22']
+    suffixes = ['!', '22']
+    pattern = 'jihg'
+    for password, i, j in genpws(pattern, prefixes, suffixes):
+        matches = matching.sequence_match(password)
+        msg = 'matches embedded sequence patterns'
+        check_matches(msg, matches, 'sequence', [pattern], [[i, j]],
+                      {
+                          'sequence_name': ['lower'],
+                          'ascending': [False],
+                      })
+
+    for [pattern, name, is_ascending] in [
+        ['ABC', 'upper', True],
+        ['CBA', 'upper', False],
+        ['PQR', 'upper', True],
+        ['RQP', 'upper', False],
+        ['XYZ', 'upper', True],
+        ['ZYX', 'upper', False],
+        ['abcd', 'lower', True],
+        ['dcba', 'lower', False],
+        ['jihg', 'lower', False],
+        ['wxyz', 'lower', True],
+        ['zxvt', 'lower', False],
+        ['0369', 'digits', True],
+        ['97531', 'digits', False],
+    ]:
+        matches = matching.sequence_match(pattern)
+        msg = "matches '#{pattern}' as a '#{name}' sequence"
+        check_matches(msg, matches, 'sequence', [pattern],
+                      [[0, len(pattern) - 1]],
+                      {
+                          'sequence_name': [name],
+                          'ascending': [is_ascending],
+                      })
+
+
+def test_repeat_matching():
+    for password in ['', '#']:
+        msg = "doesn't match length-%s repeat patterns" % len(password)
+        assert matching.repeat_match(password) == [], msg
+
+    # test single-character repeats
+    prefixes = ['@', 'y4@']
+    suffixes = ['u', 'u%7']
+    pattern = '&&&&&'
+    for [password, i, j] in genpws(pattern, prefixes, suffixes):
+        matches = matching.repeat_match(password)
+        msg = "matches embedded repeat patterns"
+        check_matches(msg, matches, 'repeat', [pattern], [[i, j]],
+                      {'base_token': ['&']})
+
+    for length in [3, 12]:
+        for chr in ['a', 'Z', '4', '&']:
+            pattern = chr * (length + 1)
+            matches = matching.repeat_match(pattern)
+            msg = "matches repeats with base character '#{chr}'"
+            check_matches(msg, matches, 'repeat', [pattern],
+                          [[0, len(pattern) - 1]],
+                          {'base_token': [chr]})
+
+    matches = matching.repeat_match('BBB1111aaaaa@@@@@@')
+    patterns = ['BBB', '1111', 'aaaaa', '@@@@@@']
+    msg = 'matches multiple adjacent repeats'
+    check_matches(msg, matches, 'repeat', patterns,
+                  [[0, 2], [3, 6], [7, 11], [12, 17]],
+                  {'base_token': ['B', '1', 'a', '@']})
+
+    matches = matching.repeat_match('2818BBBbzsdf1111@*&@!aaaaaEUDA@@@@@@1729')
+    msg = 'matches multiple repeats with non-repeats in-between'
+    check_matches(msg, matches, 'repeat', patterns,
+                  [[4, 6], [12, 15], [21, 25], [30, 35]],
+                  {'base_token': ['B', '1', 'a', '@']})
+
+    # test multi-character repeats
+    pattern = 'abab'
+    matches = matching.repeat_match(pattern)
+    msg = 'matches multi-character repeat pattern'
+    check_matches(msg, matches, 'repeat', [pattern], [[0, len(pattern) - 1]],
+                  {'base_token': ['ab']})
+
+    pattern = 'aabaab'
+    matches = matching.repeat_match(pattern)
+    msg = 'matches aabaab as a repeat instead of the aa prefix'
+    check_matches(msg, matches, 'repeat', [pattern], [[0, len(pattern) - 1]],
+                  {'base_token': ['aab']})
+
+    pattern = 'abababab'
+    matches = matching.repeat_match(pattern)
+    msg = 'identifies ab as repeat string, even though abab is also repeated'
+    check_matches(msg, matches, 'repeat', [pattern], [[0, len(pattern) - 1]],
+                  {'base_token': ['ab']})
+
+
+def test_regex_matching():
+    for pattern, name in [
+        ['1922', 'recent_year'],
+        ['2017', 'recent_year'],
+    ]:
+        matches = matching.regex_match(pattern)
+        msg = "matches #{pattern} as a #{name} pattern"
+        check_matches(msg, matches, 'regex', [pattern],
+                      [[0, pattern.length - 1]],
+                      {'regex_name': [name]})
+
+
+def date_matching():
+    for sep in ['', ' ', '-', '/', '\\', '_', '.']:
+        password = "13%s2%s1921" % sep, sep
+        matches = matching.date_match(password)
+        msg = "matches dates that use '%s' as a separator" % sep
+        check_matches(msg, matches, 'date', [password],
+                      [[0, len(password) - 1]],
+                      {
+                          'separator': [sep],
+                          'year': [1921],
+                          'month': [2],
+                          'day': [13],
+                      })
+
+    for order in ['mdy', 'dmy', 'ymd', 'ydm']:
+        d, m, y = 8, 8, 88
+        password = order.replace('y', y).replace('m', m).replace('d', d)
+        matches = matching.date_match(password)
+        msg = "matches dates with '%s' format" % order
+        check_matches(msg, matches, 'date', [password],
+                      [[0, len(password) - 1]],
+                      {
+                          'separator': [''],
+                          'year': [1988],
+                          'month': [8],
+                          'day': [8],
+                      })
+
+    password = '111504'
+    matches = matching.date_match(password)
+    msg = "matches the date with year closest to REFERENCE_YEAR when ambiguous"
+    check_matches(msg, matches, 'date', [password], [[0, len(password) - 1]],
+                  {
+                      'separator': [''],
+                      'year': [2004],  # picks '04' -> 2004 as year, not '1504'
+                      'month': [11],
+                      'day': [15],
+                  })
+
+    for day, month, year in [
+        [1, 1, 1999],
+        [11, 8, 2000],
+        [9, 12, 2005],
+        [22, 11, 1551],
+    ]:
+        password = "%s%s%s" % (year, month, day)
+        matches = matching.date_match(password)
+        msg = "matches %s" % password
+        check_matches(msg, matches, 'date', [password],
+                      [[0, len(password) - 1]],
+                      {
+                          'separator': [''],
+                          'year': [year],
+                      })
+        password = "%s.%s.%s" % (year, month, day)
+        matches = matching.date_match(password)
+        msg = "matches %s" % password
+        check_matches(msg, matches, 'date', [password],
+                      [[0, len(password) - 1]],
+                      {
+                          'separator': ['.'],
+                          'year': [year],
+                      })
+
+    password = "02/02/02"
+    matches = matching.date_match(password)
+    msg = "matches zero-padded dates"
+    check_matches(msg, matches, 'date', [password], [[0, len(password) - 1]],
+                  {
+                      'separator': ['/'],
+                      'year': [2002],
+                      'month': [2],
+                      'day': [2],
+                  })
+
+    prefixes = ['a', 'ab']
+    suffixes = ['!']
+    pattern = '1/1/91'
+    for password, i, j in genpws(pattern, prefixes, suffixes):
+        matches = matching.date_match(password)
+        msg = "matches embedded dates"
+        check_matches(msg, matches, 'date', [pattern], [[i, j]],
+                      {
+                          'year': [1991],
+                          'month': [1],
+                          'day': [1],
+                      })
+
+    matches = matching.date_match('12/20/1991.12.20')
+    msg = "matches overlapping dates"
+    check_matches(msg, matches, 'date', ['12/20/1991', '1991.12.20'],
+                  [[0, 9], [6, 15]],
+                  {
+                      'separator': ['/', '.'],
+                      'year': [1991, 1991],
+                      'month': [12, 12],
+                      'day': [20, 20],
+                  })
+
+    matches = matching.date_match('912/20/919')
+    msg = "matches dates padded by non-ambiguous digits"
+    check_matches(msg, matches, 'date', ['12/20/91'], [[1, 8]],
+                  {
+                      'separator': ['/'],
+                      'year': [1991],
+                      'month': [12],
+                      'day': [20],
+                  })
+
+
+def test_omnimatch():
+    assert matching.omnimatch('') == [], "doesn't match ''"
+    password = 'r0sebudmaelstrom11/20/91aaaa'
+    matches = matching.omnimatch(password)
+    for [pattern_name, [i, j]] in [
+        ['dictionary', [0, 6]],
+        ['dictionary', [7, 15]],
+        ['date', [16, 23]],
+        ['repeat', [24, 27]],
+    ]:
+        included = False
+        for match in matches:
+            if match.i == i and match.j == j and match.pattern == pattern_name:
+                included = True
+        msg = "for %s, matches a %s pattern at [%s, %s]" % (
+        password, pattern_name, i, j)
+        assert included, msg
