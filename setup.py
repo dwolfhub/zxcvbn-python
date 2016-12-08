6bf933f074e72783d664063e3c80208ad455d41a
Daniel Wolf <danielrwolf5@gmail.com>
2016-12-08 13:59:22 -0600

    added package configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='zxcvbn-python',
+    version='1.0.0',
+    packages=['zxcvbn'],
+    url='https://github.com/dwolfhub/zxcvbn-python',
+    download_url='https://github.com/dwolfhub/zxcvbn-python/tarball/1.0.0',
+    license='MIT',
+    author='Daniel Wolf',
+    author_email='danielrwolf5@gmail.com',
+    description='python port of Dropbox\'s zxcvbn library',
+)
