edae281d6243781548b00b22f79f293fc68d8ff3
Daniel Wolf <danielrwolf5@gmail.com>
2017-01-09 16:06:17 -0600

    upgrading setup file to version 4.4.12

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='zxcvbn-python',
-    version='4.4.11',
+    version='4.4.12',
     packages=['zxcvbn'],
     url='https://github.com/dwolfhub/zxcvbn-python',
-    download_url='https://github.com/dwolfhub/zxcvbn-python/tarball/4.4.11',
+    download_url='https://github.com/dwolfhub/zxcvbn-python/tarball/4.4.12',
     license='MIT',
     author='Daniel Wolf',
     author_email='danielrwolf5@gmail.com',
