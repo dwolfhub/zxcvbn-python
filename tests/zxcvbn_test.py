3fe6ad13ac0c4d5c5392f3cbb003bb14d70b2e5e
jason <jason@zzq.org>
2017-06-14 11:26:36 -0600

    Fix exception when user_inputs contains unicode characters
    zxcvbn shouldn't fail when the user inputs are unicode. This encodes strings as
    utf-8 rather than ascii.
    
    fixes: #12

---
+++ tests/zxcvbn_test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from zxcvbn import zxcvbn
+
+
+def test_unicode_user_inputs():
+    # test Issue #12 -- don't raise a UnicodeError with unicode user_inputs or
+    # passwords.
+    input_ = u'Фамилия'
+    password = u'pÄssword junkiË'
+
+    zxcvbn(password, user_inputs=[input_])
+
+
+def test_invalid_user_inputs():
+    # don't raise an error with non-string types for user_inputs
+    input_ = None
+    password = u'pÄssword junkiË'
+
+    zxcvbn(password, user_inputs=[input_])
