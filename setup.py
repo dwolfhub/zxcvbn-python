b09dfaf4c51f0a5dc8fb021b096872fd61f4405a
Daniel Wolf <danielrwolf5@gmail.com>
2017-06-20 16:49:53 -0500

    updated setup.py file

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='zxcvbn-python',
-    version='4.4.14',
+    version='4.4.15',
     packages=['zxcvbn'],
     url='https://github.com/dwolfhub/zxcvbn-python',
-    download_url='https://github.com/dwolfhub/zxcvbn-python/tarball/v4.4.14',
+    download_url='https://github.com/dwolfhub/zxcvbn-python/tarball/v4.4.15',
     license='MIT',
     author='Daniel Wolf',
     author_email='danielrwolf5@gmail.com',
