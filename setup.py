34e75ef2da722f7c861fff872fb933fe23bb1d5d
Jon Banafato <jon@jonafato.com>
2017-06-29 12:52:58 -0400

    Add Trove classifiers
    Provide project metadata via Trove classifiers. References:
    
    - http://www.python.org/dev/peps/pep-0301/#distutils-trove-classification
    - http://www.catb.org/~esr/trove/

--- setup.py
+++ setup.py
@@ -12,4 +12,19 @@ setup(
     description='Python implementation of Dropbox\'s realistic password '
                 'strength estimator, zxcvbn',
     keywords=['zxcvbn', 'password', 'security'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
