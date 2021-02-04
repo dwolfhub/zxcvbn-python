d1e2aa7d79d0ca8e7a5c0dcc191c6e6a98b6af93
Stephen J. Fuhry <steve@tpastream.com>
2021-02-04 19:25:27 +0000

    github actions & mypy

--- setup.py
+++ setup.py
@@ -26,10 +26,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Security',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
